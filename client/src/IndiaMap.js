f9ada6625369a227a7342537708191077b49a33b
Abhishek Yadav <31823769+abhishekyad@users.noreply.github.com>
2022-02-08 12:20:45 +0530

    Add files via upload

---
+++ client/src/IndiaMap.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import India from "@svg-maps/india";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import data from './data.json';
+
+function IndiaMap(){
+  const [stateCode, setStateCode] = useState("");
+  const [stateName, setStateName] = useState("");
+  const [i, seti] = useState("");
+  const [j, setj] = useState("");
+  const [k, setk] = useState("");
+
+
+  function onLocationClick(event) {
+    setStateCode(event.target.id);
+    setStateName(event.target.getAttribute("name"));
+    // console.log(event.target);
+    console.log("Id", event.target.id);
+    console.log("Name",event.target.getAttribute("name"));
+    var str=event.target.getAttribute("name");
+    console.log(data[str]);
+    seti("Active Cases : "+data[str].active);
+    setj("Cases Cured : "+data[str].cured);
+    setk("Deaths : "+data[str].deaths);
+  }
+
+  return (
+    <><p> State-wise Count</p>
+      <SVGMap map={India} onLocationClick={onLocationClick} />
+      <p>{stateName}</p>
+      <p>{i}</p>
+      <p>{j}</p>
+      <p>{k}</p>
+    </>
+  );
+}
+
+export default IndiaMap;
---
+++ client/src/List.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import Panel from 'react-bootstrap/lib/Panel'
+import axios from 'axios'
+import './App.css';
+import data from './data.json'
+
+//This Component is a child Component of Customers Component
+export default class CustomerDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  //Function which is called when the component loads for the first time
+  componentDidMount() {
+    this.getCustomerDetails(this.props.val)
+  }
+
+  //Function which is called whenver the component is updated
+  componentDidUpdate(prevProps) {
+    //get Customer Details only if props has changed
+    if (this.props.val !== prevProps.val) {
+      this.getCustomerDetails(this.props.val)
+    }
+  }
+
+  //Function to Load the customerdetails data from json.
+  getCustomerDetails(id) {
+    axios.get('assets/samplejson/customer' + id + '.json').then(response => {
+      this.setState({customerDetails: response})
+    })
+  };
+
+  render() {
+    if (!this.state.customerDetails)
+      return (<p>Loading Data</p>)
+    return (<div className="count">
+      <Panel bsStyle="info" className="centeralign">
+        <Panel.Heading>
+          <Panel.Title componentClass="h3">{'National Count'}</Panel.Title>
+        </Panel.Heading>
+        <Panel.Body className="cases">
+          <p>Active Cases : {data.active}</p>
+          <p>Discharged : {data.discharged}</p>
+          <p>Deaths : {data.deaths}</p>
+          <p> Total Vaccination : {data.vaccination}</p>
+        </Panel.Body>
+      </Panel>
+    </div>)
+  }
+}
