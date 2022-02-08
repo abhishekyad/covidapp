1ac1202a4fdc4ff4929886446fc3ad170d8192fd
GG <abhisheky57@gmail.com>
2022-02-08 11:32:44 +0530

    initial commit

---
+++ covidapp/client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customers from './Customers'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import IndiaMap from "./IndiaMap";
+import mask from './mask.png';
+import inshorts from './inshorts.png';
+
+class App extends Component {
+  render() {
+    console.log("Host URL"+process.env.PUBLIC_URL);
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Win Over COVID</h1>
+          <h1 className="App-title2"> ... and InShorts too!</h1>
+          <img src={inshorts} className="icon"/>
+        </header>
+        <div className="hori">
+          <Switch >
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/customerlist"/>
+                )}/>
+                 <Route exact path='/customerlist' component={Customers} />
+          </Switch>
+        <div  className="App-map">
+        <IndiaMap/>
+        </div >
+        <div className="mask">
+        <p>We Shall Overcome</p>
+        <img src={mask}/>
+        </div>
+        </div>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
