8e8fbb4f7f2d8d14a500bd9e6dda6c39784af3aa
Abhishek Yadav <31823769+abhishekyad@users.noreply.github.com>
2022-03-14 10:47:05 +0530

    Yes

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Win Over COVID</h1>
-          <h1 className="App-title2"> ... and InShorts too!</h1>
-          <img src={inshorts} className="icon"/>
         </header>
         <div className="hori">
           <Switch >
